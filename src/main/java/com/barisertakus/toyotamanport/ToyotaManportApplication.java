63229878ee992c88756f44c50956a95af8ecfcd3
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-01-04 23:34:03 +0300

    Initial commit.

---
+++ src/main/java/com/barisertakus/toyotamanport/ToyotaManportApplication.java
@@ -0,0 +1,13 @@
+package com.barisertakus.toyotamanport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyotaManportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyotaManportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/barisertakus/toyotamanport/ToyotaManportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barisertakus.toyotamanport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyotaManportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
