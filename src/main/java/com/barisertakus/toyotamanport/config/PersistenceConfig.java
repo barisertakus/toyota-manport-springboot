da1d896c8faec4ce015cced013f6b3907cc9e7c8
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-01-12 02:31:03 +0300

    fix PersistenceConfig authentication null error

--- src/main/java/com/barisertakus/toyotamanport/config/PersistenceConfig.java
+++ src/main/java/com/barisertakus/toyotamanport/config/PersistenceConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
@@ -16,7 +17,12 @@ public class PersistenceConfig {
     @Bean
     public AuditorAware<String> auditorAware(){
         return () -> {
-            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            Object principal = null;
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if(authentication != null){
+                principal = authentication.getPrincipal();
+            }
 
             if(principal instanceof UserDetailsImpl){
                 String username = ((UserDetailsImpl) principal).getUsername();
