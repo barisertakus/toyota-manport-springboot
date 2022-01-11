19fdf5a084c01a45039d95b75a0d069c9ae92004
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-01-12 02:35:39 +0300

    add User list to Role entity

--- src/main/java/com/barisertakus/toyotamanport/dto/SignupRequest.java
+++ src/main/java/com/barisertakus/toyotamanport/dto/SignupRequest.java
@@ -1,10 +1,14 @@
 package com.barisertakus.toyotamanport.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignupRequest {
     private String username;
     private String name;
--- src/main/java/com/barisertakus/toyotamanport/entity/Role.java
+++ src/main/java/com/barisertakus/toyotamanport/entity/Role.java
@@ -1,12 +1,16 @@
 package com.barisertakus.toyotamanport.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
 @Table
+@NoArgsConstructor
 public class Role extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,4 +18,11 @@ public class Role extends BaseEntity{
 
     @Column(length = 20)
     private String name;
+
+    @ManyToMany
+    private List<User> users;
+
+    public Role(String name){
+        this.name = name;
+    }
 }
