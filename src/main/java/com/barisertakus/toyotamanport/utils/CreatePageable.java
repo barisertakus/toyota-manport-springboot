2d5f4fb04012f40418ba3c4ca7122901d2635444
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-01-13 23:48:05 +0300

    add CreatePageable to Utils

---
+++ src/main/java/com/barisertakus/toyotamanport/utils/CreatePageable.java
@@ -0,0 +1,28 @@
+package com.barisertakus.toyotamanport.utils;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.Objects;
+
+public class CreatePageable {
+    public static Pageable create(int pageNo, int pageSize, String sortType, String sortField) {
+        Sort sort = null;
+        boolean sortFieldEmpty = sortField != null && !sortField.isEmpty();
+        if ("asc".equals(sortType) && !sortFieldEmpty) {
+            sort = Sort.by(Sort.Direction.ASC, sortField);
+        } else if ("desc".equals(sortType) && !sortFieldEmpty)
+            sort = Sort.by(Sort.Direction.DESC, sortField);
+
+        Pageable pageable;
+
+        if (!Objects.isNull(sort)) {
+            pageable = PageRequest.of(pageNo, pageSize, sort);
+        } else {
+            pageable = PageRequest.of(pageNo, pageSize);
+        }
+
+        return pageable;
+    }
+}
