ccc6917961d4a8e69c050331756771dd3e75cb18
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-01-23 02:42:06 +0300

    add findByValue to all enums

--- src/main/java/com/barisertakus/toyotamanport/enums/ServerType.java
+++ src/main/java/com/barisertakus/toyotamanport/enums/ServerType.java
@@ -2,5 +2,13 @@ package com.barisertakus.toyotamanport.enums;
 
 public enum ServerType {
     PRODUCTION,
-    TEST
+    TEST;
+
+    public ServerType findByValue(String value){
+        for(ServerType serverType : ServerType.values()){
+            if(value.equals(serverType.toString()))
+                return serverType;
+        }
+        return null;
+    }
 }
