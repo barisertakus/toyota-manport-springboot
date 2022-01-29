e2659b68867da099b14deecceaf5b4bd8d5092a8
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-01-30 01:56:25 +0300

    remove applicationServerService

--- src/main/java/com/barisertakus/toyotamanport/repository/ApplicationServerIssueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.barisertakus.toyotamanport.repository;
-
-import com.barisertakus.toyotamanport.entity.ApplicationServerIssue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationServerIssueRepository extends JpaRepository<ApplicationServerIssue, Long> {
-}
