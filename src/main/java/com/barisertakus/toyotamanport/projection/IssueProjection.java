d1dd31005d765f163ef57e9af88530560077e3dc
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-02-10 03:03:59 +0300

    update IssueProjection fields

--- src/main/java/com/barisertakus/toyotamanport/projection/IssueProjection.java
+++ src/main/java/com/barisertakus/toyotamanport/projection/IssueProjection.java
@@ -2,23 +2,20 @@ package com.barisertakus.toyotamanport.projection;
 
 
 import com.barisertakus.toyotamanport.enums.ImpactType;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
 
 public interface IssueProjection {
     Long getId();
 
-    Date getCreatedDate();
+    Date getUpdatedDate();
 
-    @Value("#{target.applicationPlant.application.shortName}")
     String getApplicationName();
 
     String getIssueType();
 
     ImpactType getImpactType();
 
-    @Value("#{target.applicationPlant.plant.country}")
     String getPlantCountry();
 
     String getDescription();
